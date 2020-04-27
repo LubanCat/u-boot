7bcaf050a3c5a40afbc02057782d01d610ceb5d9
Patrick Delaunay <patrick.delaunay@st.com>
2020-04-27 15:30:01 +0200

    UPSTREAM: usb: host: dwc2: add trace to have clean usb start
    Solve issue for the display of "usb start" command on stm32mp1
    because one carriage return is missing in DWC2 probe.
    
    Before the patch:
    
    STM32MP> usb start
    starting USB...
    Bus usb-otg@49000000:    Bus usbh-ehci@5800d000:   USB EHCI 1.00
    
    after the patch:
    
    STM32MP> usb start
    starting USB...
    Bus usb-otg@49000000: USB DWC2
    Bus usbh-ehci@5800d000: USB EHCI 1.00
    
    Change-Id: I821e082679cacfc53d49b95882982983810c42de
    Signed-off-by: Patrick Delaunay <patrick.delaunay@st.com>
    Signed-off-by: Frank Wang <frank.wang@rock-chips.com>
    (cherry picked from commit 245847f06570a74ecff59809a2befc1dce7a354e)

--- drivers/usb/host/dwc2.c
+++ drivers/usb/host/dwc2.c
@@ -1220,6 +1220,8 @@ static int dwc2_init_common(struct udevice *dev, struct dwc2_priv *priv)
 	if (readl(&regs->gintsts) & DWC2_GINTSTS_CURMODE_HOST)
 		mdelay(1000);
 
+	printf("USB DWC2\n");
+
 	return 0;
 }
 

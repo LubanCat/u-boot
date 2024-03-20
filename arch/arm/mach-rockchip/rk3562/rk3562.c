LubanCat/u-boot

commit bba150994e73f9169a84df40e3f6720bbd2c57e4

usbplug: rk3562: Support SPI Flash
Change-Id: Ife2654531fe5fe0991e4673542a52da6eb5eced1
Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

arch/arm/mach-rockchip/rk3562/rk3562.c
@@ -529,6 +529,19 @@ int fit_standalone_release(char *id, uintptr_t entry_point)
 	return 0;
 }
 
+void board_set_iomux(enum if_type if_type, int devnum, int routing)
+{
+	switch (if_type) {
+	case IF_TYPE_MTD:
+		/* FSPI */
+		writel(0xffff2222, GPIO1_IOC_BASE + GPIO1A_IOMUX_SEL_L);
+		writel(0x00ff0022, GPIO1_IOC_BASE + GPIO1B_IOMUX_SEL_L);
+		break;
+	default:
+		printf("Bootdev 0x%x is not support\n", if_type);
+	}
+}
+
 #if defined(CONFIG_SPL_BUILD) && !defined(CONFIG_TPL_BUILD)
 static void qos_priority_init(void)
 {
@@ -611,6 +624,7 @@ static void qos_priority_init(void)
 
 	writel(0x5, PCIE_SHAPING_REG);
 }
+#endif
 
 int arch_cpu_init(void)
 {
@@ -667,8 +681,9 @@ int arch_cpu_init(void)
 	}
 #endif
 
+#if defined(CONFIG_SPL_BUILD) && !defined(CONFIG_TPL_BUILD)
 	qos_priority_init();
+#endif
 
 	return 0;
 }
-#endif

configs/rk3562-usbplug.config
@@ -1,25 +1,86 @@
+CONFIG_ARM64=y
+CONFIG_ARM_PSCI_FW=y
+CONFIG_ARM_SMCCC=y
+# CONFIG_ARMV8_CRYPTO is not set
+# CONFIG_ARMV8_MULTIENTRY is not set
+# CONFIG_ARMV8_PSCI is not set
+# CONFIG_ARMV8_SEC_FIRMWARE_SUPPORT is not set
+# CONFIG_ARMV8_SET_SMPEN is not set
 CONFIG_BASE_DEFCONFIG="rockchip-usbplug_defconfig"
-# CONFIG_TARGET_EVB_RV1126 is not set
-# CONFIG_ROCKCHIP_RV1126 is not set
+CONFIG_CMD_BOOTI=y
 CONFIG_DEBUG_UART_BASE=0xff210000
 CONFIG_DEFAULT_DEVICE_TREE="rk3562-evb"
-CONFIG_ROCKCHIP_RK3562=y
-CONFIG_TARGET_EVB_RK3562=y
-CONFIG_MMC_SDHCI=y
-CONFIG_MMC_SDHCI_SDMA=y
-CONFIG_MMC_SDHCI_ROCKCHIP=y
-CONFIG_ROCKCHIP_NEW_IDB=y
-# CONFIG_CLK_SCMI is not set
-# CONFIG_SCMI_FIRMWARE is not set
-# CONFIG_MMC_DW is not set
-# CONFIG_MMC_DW_ROCKCHIP is not set
-CONFIG_ROCKCHIP_BOOT_MODE_REG=0xff010220
-CONFIG_ROCKCHIP_STIMER_BASE=0xffa90020
-CONFIG_ROCKCHIP_IRAM_START_ADDR=0xfe480000
-# CONFIG_ROCKCHIP_SFC is not set
-# CONFIG_MTD is not set
-# CONFIG_MTD_BLK is not set
-# CONFIG_MTD_DEVICE is not set
-# CONFIG_SPI_FLASH is not set
-CONFIG_PHY_ROCKCHIP_NANENG_COMBOPHY=y
+CONFIG_DMA_ADDR_T_64BIT=y
 CONFIG_DM_RESET=y
+CONFIG_FIRMWARE=y
+CONFIG_GICV2=y
+CONFIG_IRQ=y
+# CONFIG_MMC_DW is not set
+# CONFIG_MMC_SDHCI_BCMSTB is not set
+# CONFIG_MMC_SDHCI_CADENCE is not set
+# CONFIG_MMC_SDHCI_KONA is not set
+# CONFIG_MMC_SDHCI_MSM is not set
+CONFIG_MMC_SDHCI_ROCKCHIP=y
+# CONFIG_MMC_SDHCI_S5P is not set
+CONFIG_MMC_SDHCI_SDMA=y
+# CONFIG_MMC_SDHCI_SPEAR is not set
+# CONFIG_MMC_SDHCI_STI is not set
+# CONFIG_MMC_SDHCI_TANGIER is not set
+# CONFIG_MMC_SDHCI_XENON is not set
+CONFIG_MMC_SDHCI=y
+CONFIG_MTD_NAND_BBT_USING_FLASH=y
+CONFIG_MTD_NAND_CORE=y
+CONFIG_MTD_SPI_NAND=y
+# CONFIG_NOP_PHY is not set
+CONFIG_PHY_ROCKCHIP_NANENG_COMBOPHY=y
+CONFIG_PHYS_64BIT=y
+CONFIG_PHY=y
+# CONFIG_POSITION_INDEPENDENT is not set
+CONFIG_PSCI_RESET=y
+CONFIG_RESET_ROCKCHIP=y
+CONFIG_ROCKCHIP_BOOT_MODE_REG=0xff010220
+# CONFIG_ROCKCHIP_DEBUGGER is not set
+CONFIG_ROCKCHIP_IRAM_START_ADDR=0xfe480000
+CONFIG_ROCKCHIP_NEW_IDB=y
+CONFIG_ROCKCHIP_RK3562=y
+# CONFIG_ROCKCHIP_RV1126 is not set
+CONFIG_ROCKCHIP_STIMER_BASE=0xffa90020
+# CONFIG_SCMI_FIRMWARE is not set
+CONFIG_SPI_FLASH_DOSILICON=y
+CONFIG_SPI_FLASH_FMSH=y
+# CONFIG_SPI_FLASH_MACRONIX is not set
+# CONFIG_SPI_FLASH_MTD is not set
+CONFIG_SPI_FLASH_PUYA=y
+CONFIG_SPI_FLASH_XTX=y
+CONFIG_SPI_NAND_BIWIN=y
+CONFIG_SPI_NAND_DOSILICON=y
+CONFIG_SPI_NAND_ESMT=y
+CONFIG_SPI_NAND_ETRON=y
+CONFIG_SPI_NAND_FMSH=y
+CONFIG_SPI_NAND_FORESEE=y
+CONFIG_SPI_NAND_GIGADEVICE=y
+CONFIG_SPI_NAND_GSTO=y
+CONFIG_SPI_NAND_HYF=y
+CONFIG_SPI_NAND_JSC=y
+CONFIG_SPI_NAND_MACRONIX=y
+CONFIG_SPI_NAND_MICRON=y
+CONFIG_SPI_NAND_SILICONGO=y
+CONFIG_SPI_NAND_SKYHIGH=y
+CONFIG_SPI_NAND_TOSHIBA=y
+CONFIG_SPI_NAND_UNIM=y
+CONFIG_SPI_NAND_WINBOND=y
+CONFIG_SPI_NAND_XINCUN=y
+CONFIG_SPI_NAND_XTX=y
+# CONFIG_SPL_ARMV8_SEC_FIRMWARE_SUPPORT is not set
+CONFIG_SPL_LDSCRIPT="arch/arm/cpu/armv8/u-boot-spl.lds"
+# CONFIG_SPL_SCMI_FIRMWARE is not set
+CONFIG_STATIC_RELA=y
+CONFIG_SYS_ARM_ARCH=8
+CONFIG_SYS_BOARD="evb_rk3562"
+CONFIG_SYS_CONFIG_NAME="evb_rk3562"
+CONFIG_SYS_CPU="armv8"
+CONFIG_SYSRESET_PSCI=y
+CONFIG_TARGET_EVB_RK3562=y
+CONFIG_TPL_LDSCRIPT="arch/arm/mach-rockchip/u-boot-tpl-v8.lds"
+CONFIG_TPL_MAX_SIZE=61440
+CONFIG_TPL_TEXT_BASE=0xfe481000

commit 34056e9cc852e4497ac46ae98e83d7c3fc30c66e

rockchip: rk3562: Fix error in secure setting
Only set secure configuration in SPL/USBPLUG.

Fixes: bba150994 ("usbplug: rk3562: Support SPI Flash")
Change-Id: Ieabd6e1d96d8a88857e5a59d47cc03cb45f6b02b
Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

arch/arm/mach-rockchip/rk3562/rk3562.c
@@ -628,6 +628,7 @@ static void qos_priority_init(void)
 
 int arch_cpu_init(void)
 {
+#if defined(CONFIG_SPL_BUILD) || defined(CONFIG_SUPPORT_USBPLUG)
 	u32 val;
 
 	/* Set the emmc to access ddr memory */
@@ -681,9 +682,10 @@ int arch_cpu_init(void)
 	}
 #endif
 
-#if defined(CONFIG_SPL_BUILD) && !defined(CONFIG_TPL_BUILD)
+#if !defined(CONFIG_TPL_BUILD)
 	qos_priority_init();
 #endif
+#endif /* #if defined(CONFIG_SPL_BUILD) || defined(CONFIG_SUPPORT_USBPLUG) */
 
 	return 0;
 }
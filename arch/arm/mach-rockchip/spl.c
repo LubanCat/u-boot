bff96653d33ed531df73677f3f35e46310572669
Xuhui Lin <xuhui.lin@rock-chips.com>
2024-04-26 10:57:18 +0800

    rockchip: spl: Fix code bug generate from commit c07b486e4fc
    Brief commit message:
    c07b486e4fc rockchip: spl: Improve handle of boot mode reg info when
    deciding next stage
    
    While push this commit, mistakenly delete break;
    behind deciding next_stage is U-Boot from boot mode reg.
    
    Change-Id: I07a9b77f1ba8db22734524225e6a6f9957250403
    Signed-off-by: Xuhui Lin <xuhui.lin@rock-chips.com>

--- arch/arm/mach-rockchip/spl.c
+++ arch/arm/mach-rockchip/spl.c
@@ -432,6 +432,7 @@ void spl_next_stage(struct spl_image_info *spl)
 	case BOOT_DFU:
 		i = 3;
 		spl->next_stage = SPL_NEXT_STAGE_UBOOT;
+		break;
 	default:
 		spl->next_stage = SPL_NEXT_STAGE_KERNEL;
 	}

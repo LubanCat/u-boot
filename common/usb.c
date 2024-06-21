LubanCat/u-boot

commit cae1d03732fca28a0352bd09c4a9d51f538bdb31

common: usb: fix endpoints number in interface descriptor
Some odd devices respond the Endpoint descriptor items are less then the
bNumEndpoints in Interface descriptor, so fix it at config parse time.

Change-Id: I7d4f21d45904552d1f8fb81abf3a2183c17f504e
Signed-off-by: Frank Wang <frank.wang@rock-chips.com>

common/usb.c
@@ -517,6 +517,20 @@ static int usb_parse_config(struct usb_device *dev,
 		index += head->bLength;
 		head = (struct usb_descriptor_header *)&buffer[index];
 	}
+
+	/**
+	 * Some odd devices respond the Endpoint descriptor items are less
+	 * then the bNumEndpoints in Interface descriptor, so fix it here.
+	 */
+	for (ifno = 0; ifno < dev->config.no_of_if; ifno++) {
+		if_desc = &dev->config.if_desc[ifno];
+		if (if_desc->desc.bNumEndpoints != if_desc->no_of_ep) {
+			printf("WARN: interface %d has %d endpoint descriptor, "
+			       "different from the interface descriptor's value: %d\n",
+			       ifno, if_desc->no_of_ep, if_desc->desc.bNumEndpoints);
+			if_desc->desc.bNumEndpoints = if_desc->no_of_ep;
+		}
+	}
 	return 0;
 }
 

commit 3d03df18eadb51e5106f1e566b524818fb6c3e89

common: usb: fixed a few u-disk enumeration failures
A few U-Disk likes ASMedia(VID=174cH, PID=55aaH) failed to enumerate
as the endpoint descriptors reported is greater than the interface
descriptor's bNumEndpoints, and the following warning is output:

=> usb start
starting USB...
[...]
scanning bus usb@23000000 for devices... 1 USB Device(s) found
scanning bus usb@23400000 for devices... WARN: interface 0 has 6
endpoint descriptor, different from the interface descriptor's value: 2
WARN halted endpoint, queueing URB anyway.
Unexpected XHCI event TRB, skipping... (fbd68570 00000000 13000000 01008401)
WARN halted endpoint, queueing URB anyway.

Fixes: cae1d03732fc ("common: usb: fix endpoints number in interface descriptor")
Change-Id: I9fd019262624b872545f59185b18f39df966982f
Signed-off-by: Frank Wang <frank.wang@rock-chips.com>

common/usb.c
@@ -521,15 +521,15 @@ static int usb_parse_config(struct usb_device *dev,
 		head = (struct usb_descriptor_header *)&buffer[index];
 	}
 
-	/**
+	/*
 	 * Some odd devices respond the Endpoint descriptor items are less
-	 * then the bNumEndpoints in Interface descriptor, so fix it here.
+	 * than the bNumEndpoints in Interface descriptor, so fix it here.
 	 */
 	for (ifno = 0; ifno < dev->config.no_of_if; ifno++) {
 		if_desc = &dev->config.if_desc[ifno];
-		if (if_desc->desc.bNumEndpoints != if_desc->no_of_ep) {
+		if (if_desc->no_of_ep < if_desc->desc.bNumEndpoints) {
 			printf("WARN: interface %d has %d endpoint descriptor, "
-			       "different from the interface descriptor's value: %d\n",
+			       "less than the interface descriptor's value: %d\n",
 			       ifno, if_desc->no_of_ep, if_desc->desc.bNumEndpoints);
 			if_desc->desc.bNumEndpoints = if_desc->no_of_ep;
 		}
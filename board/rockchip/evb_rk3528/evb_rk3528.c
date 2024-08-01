LubanCat/u-boot

commit 14d5da7dccb82fa3b5358431830e247785108782

phy: rockchip: naneng-combphy: Add usb3 phy init for rockusb
This patch init usb3 phy for rockusb gadget to support super
speed download image. Note that if usb3 phy has been initializd
successfully, we should select clk_usb3otg0_pipe as the source
clock for usb3 otg controller. And if it fail to init usb3 phy,
we should select clk_usb3otg0_utmi as the source clock for usb3
otg controller.

Signed-off-by: Jianwei Zheng <jianwei.zheng@rock-chips.com>
Signed-off-by: william.wu <william.wu@rock-chips.com>
Change-Id: I39d5c863f4f54b27343d033aaea73fb39f873c0d

drivers/phy/phy-rockchip-naneng-combphy.c
@@ -16,6 +16,7 @@
 #include <asm/arch/clock.h>
 #include <regmap.h>
 #include <reset-uclass.h>
+#include <linux/iopoll.h>
 
 #define BIT_WRITEABLE_SHIFT		16
 
@@ -63,6 +64,7 @@ struct rockchip_combphy_grfcfg {
 	struct combphy_reg pipe_xpcs_phy_ready;
 	struct combphy_reg u3otg0_port_en;
 	struct combphy_reg u3otg1_port_en;
+	struct combphy_reg u3otg0_pipe_clk_sel;
 	struct combphy_reg pipe_phy_grf_reset;
 };
 
@@ -93,6 +95,20 @@ static int param_write(struct regmap *base,
 	val = (tmp << reg->bitstart) | (mask << BIT_WRITEABLE_SHIFT);
 
 	return regmap_write(base, reg->offset, val);
+}
+
+static u32 rockchip_combphy_is_ready(struct rockchip_combphy_priv *priv)
+{
+	const struct rockchip_combphy_grfcfg *cfg = priv->cfg->grfcfg;
+	u32 mask, val;
+
+	mask = GENMASK(cfg->pipe_phy_status.bitend,
+		       cfg->pipe_phy_status.bitstart);
+
+	regmap_read(priv->phy_grf, cfg->pipe_phy_status.offset, &val);
+	val = (val & mask) >> cfg->pipe_phy_status.bitstart;
+
+	return val;
 }
 
 static int rockchip_combphy_pcie_init(struct rockchip_combphy_priv *priv)
@@ -151,6 +167,53 @@ static int rockchip_combphy_sgmii_init(struct rockchip_combphy_priv *priv)
 			return ret;
 		}
 	}
+
+	return ret;
+}
+
+int rockchip_combphy_usb3_uboot_init(void)
+{
+	struct udevice *udev;
+	struct rockchip_combphy_priv *priv;
+	const struct rockchip_combphy_grfcfg *cfg;
+	u32 val;
+	int ret;
+
+	ret = uclass_get_device_by_driver(UCLASS_PHY,
+					  DM_GET_DRIVER(rockchip_naneng_combphy),
+					  &udev);
+	if (ret) {
+		pr_err("%s: get usb3-phy node failed: %d\n", __func__, ret);
+		return ret;
+	}
+
+	priv = dev_get_priv(udev);
+	priv->mode = PHY_TYPE_USB3;
+	cfg = priv->cfg->grfcfg;
+
+	rockchip_combphy_usb3_init(priv);
+	reset_deassert(&priv->phy_rst);
+
+	if (cfg->pipe_phy_grf_reset.enable)
+		param_write(priv->phy_grf, &cfg->pipe_phy_grf_reset, false);
+
+	if (priv->mode == PHY_TYPE_USB3) {
+		ret = readx_poll_timeout(rockchip_combphy_is_ready,
+					 priv, val,
+					 val == cfg->pipe_phy_status.enable,
+					 1000);
+		if (ret) {
+			dev_err(priv->dev, "wait phy status ready timeout\n");
+			param_write(priv->phy_grf, &cfg->usb_mode_set, false);
+			if (cfg->u3otg0_pipe_clk_sel.disable)
+				param_write(priv->phy_grf, &cfg->u3otg0_pipe_clk_sel, false);
+			return ret;
+		}
+	}
+
+	/* Select clk_usb3otg0_pipe for source clk */
+	if (cfg->u3otg0_pipe_clk_sel.disable)
+		param_write(priv->phy_grf, &cfg->u3otg0_pipe_clk_sel, true);
 
 	return ret;
 }
@@ -379,6 +442,7 @@ static const struct rockchip_combphy_grfcfg rk3528_combphy_grfcfgs = {
 	.con2_for_pcie		= { 0x48008, 15, 0, 0x00, 0x101 },
 	.con3_for_pcie		= { 0x4800c, 15, 0, 0x00, 0x0200 },
 	/* pipe-grf */
+	.u3otg0_pipe_clk_sel	= { 0x40044, 7, 7, 0x01, 0x00 },
 	.u3otg0_port_en		= { 0x40044, 15, 0, 0x0181, 0x1100 },
 };
 

include/linux/usb/phy-rockchip-naneng-combphy.h
@@ -1,0 +1,18 @@
+/* SPDX-License-Identifier:     GPL-2.0 */
+/*
+ * Copyright (C) 2023 Rockchip Electronics Co., Ltd
+ */
+
+#ifndef _PHY_ROCKCHIP_NANENG_COMBPHY_H
+#define _PHY_ROCKCHIP_NANENG_COMBPHY_H
+
+#if CONFIG_IS_ENABLED(PHY_ROCKCHIP_NANENG_COMBOPHY)
+int rockchip_combphy_usb3_uboot_init(void);
+#else
+static inline int rockchip_combphy_usb3_uboot_init(void)
+{
+	return -ENOTSUPP;
+}
+#endif
+
+#endif /* _PHY_ROCKCHIP_NANENG_COMBPHY_H */

commit 9863b7ebb87dd9cd7793d3e792ebe89d223619e7

phy: rockchip: naneng-combphy: fix phy device mismatch
The uclass_get_device_by_driver() API always finds the first device in
DT probed, however, it may not the PHY that associate with USB download
controller, if mismathed, the below errors would be printed in terminal.

  download key pressed... entering download mode...
  RKUSB: LUN 0, dev 0, hwpart 0, sector 0x0, count 0x1d5a000
  failed to enable ep0out
  failed to start <NULL>: -110
  g_dnl_register: failed!, error: -110
  g_dnl_register failedEnter rockusb failed, fallback to bootrom...

So passed the PHY physical address and loop to match the correct PHY
device to fix it.

Fixes: 14d5da7dccb8 ("phy: rockchip: naneng-combphy: Add usb3 phy init for rockusb")
Change-Id: Ib65a104008d6302dca09cc081a3457b3d55e0323
Signed-off-by: Frank Wang <frank.wang@rock-chips.com>

board/rockchip/evb_rk3528/evb_rk3528.c
@@ -16,6 +16,7 @@ DECLARE_GLOBAL_DATA_PTR;
 #ifdef CONFIG_USB_DWC3
 #define CRU_BASE		0xff4a0000
 #define CRU_SOFTRST_CON33	0x0a84
+#define U3PHY_BASE		0xffdc0000
 
 static struct dwc3_device dwc3_device_data = {
 	.maximum_speed = USB_SPEED_SUPER,
@@ -57,14 +58,14 @@ int board_usb_init(int index, enum usb_init_type init)
 
 	if (rkusb_switch_usb3_enabled()) {
 		dwc3_device_data.maximum_speed = USB_SPEED_SUPER;
-		ret = rockchip_combphy_usb3_uboot_init();
+		ret = rockchip_combphy_usb3_uboot_init(U3PHY_BASE);
 		if (ret) {
 			rkusb_force_to_usb2(true);
 			dwc3_device_data.maximum_speed = USB_SPEED_HIGH;
 		}
 	}
 #else
-	ret = rockchip_combphy_usb3_uboot_init();
+	ret = rockchip_combphy_usb3_uboot_init(U3PHY_BASE);
 	if (ret) {
 		rkusb_force_to_usb2(true);
 		dwc3_device_data.maximum_speed = USB_SPEED_HIGH;

board/rockchip/evb_rk3568/evb_rk3568.c
@@ -17,6 +17,7 @@ DECLARE_GLOBAL_DATA_PTR;
 #ifdef CONFIG_USB_DWC3
 #define CRU_BASE		0xfdd20000
 #define CRU_SOFTRST_CON09	0x0424
+#define U3PHY_BASE		0xfe820000
 
 static struct dwc3_device dwc3_device_data = {
 	.maximum_speed = USB_SPEED_SUPER,
@@ -58,7 +59,7 @@ int board_usb_init(int index, enum usb_init_type init)
 
 	if (rkusb_switch_usb3_enabled()) {
 		dwc3_device_data.maximum_speed = USB_SPEED_SUPER;
-		ret = rockchip_combphy_usb3_uboot_init();
+		ret = rockchip_combphy_usb3_uboot_init(U3PHY_BASE);
 		if (ret) {
 			rkusb_force_to_usb2(true);
 			dwc3_device_data.maximum_speed = USB_SPEED_HIGH;
@@ -69,7 +70,7 @@ int board_usb_init(int index, enum usb_init_type init)
 		rkusb_force_to_usb2(true);
 		dwc3_device_data.maximum_speed = USB_SPEED_HIGH;
 	} else {
-		ret = rockchip_combphy_usb3_uboot_init();
+		ret = rockchip_combphy_usb3_uboot_init(U3PHY_BASE);
 		if (ret) {
 			rkusb_force_to_usb2(true);
 			dwc3_device_data.maximum_speed = USB_SPEED_HIGH;

drivers/phy/phy-rockchip-naneng-combphy.c
@@ -9,6 +9,7 @@
 #include <clk.h>
 #include <dm.h>
 #include <dm/lists.h>
+#include <dm/uclass-internal.h>
 #include <dt-bindings/phy/phy.h>
 #include <generic-phy.h>
 #include <syscon.h>
@@ -183,18 +184,31 @@ static int rockchip_combphy_sgmii_init(struct rockchip_combphy_priv *priv)
 	return ret;
 }
 
-int rockchip_combphy_usb3_uboot_init(void)
+int rockchip_combphy_usb3_uboot_init(fdt_addr_t phy_addr)
 {
-	struct udevice *udev;
+	struct udevice *udev = NULL;
+	struct udevice *dev;
+	struct uclass *uc;
+	const struct driver *find_drv;
 	struct rockchip_combphy_priv *priv;
 	const struct rockchip_combphy_grfcfg *cfg;
 	u32 val;
-	int ret;
+	int ret = 0;
 
-	ret = uclass_get_device_by_driver(UCLASS_PHY,
-					  DM_GET_DRIVER(rockchip_naneng_combphy),
-					  &udev);
-	if (ret) {
+	ret = uclass_get(UCLASS_PHY, &uc);
+	if (ret)
+		return ret;
+
+	find_drv = DM_GET_DRIVER(rockchip_naneng_combphy);
+	list_for_each_entry(dev, &uc->dev_head, uclass_node) {
+		if (dev->driver == find_drv && dev_read_addr(dev) == phy_addr) {
+			ret = uclass_get_device_tail(dev, 0, &udev);
+			break;
+		}
+	}
+
+	if (!udev || ret) {
+		ret = ret ? ret : -ENODEV;
 		pr_err("%s: get usb3-phy node failed: %d\n", __func__, ret);
 		return ret;
 	}

include/linux/usb/phy-rockchip-naneng-combphy.h
@@ -7,9 +7,9 @@
 #define _PHY_ROCKCHIP_NANENG_COMBPHY_H
 
 #if CONFIG_IS_ENABLED(PHY_ROCKCHIP_NANENG_COMBOPHY)
-int rockchip_combphy_usb3_uboot_init(void);
+int rockchip_combphy_usb3_uboot_init(fdt_addr_t phy_addr);
 #else
-static inline int rockchip_combphy_usb3_uboot_init(void)
+int rockchip_combphy_usb3_uboot_init(fdt_addr_t phy_addr)
 {
 	return -ENOTSUPP;
 }
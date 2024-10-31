LubanCat/u-boot

commit c54cc5d2df717610aac7930228211f5b3daef842

video/drm: vop2: Disable acm bypass when vop init
Black screen is displayed when acm bypass is switched.
To avoid black screen, acm bypass is disabled by
default after system booted.

Change-Id: I03b22cdc6f0ff6a9493d03c853548b344f046cf0
Signed-off-by: Algea Cao <algea.cao@rock-chips.com>

drivers/video/drm/rockchip_vop2.c
@@ -2903,6 +2903,40 @@ static void rockchip_vop2_sharp_init(struct vop2 *vop2, struct display_state *st
 	writel(0x1, sharp_reg_base);
 }
 
+static void rockchip_vop2_acm_init(struct vop2 *vop2, struct display_state *state)
+{
+	struct crtc_state *cstate = &state->crtc_state;
+	const struct vop2_data *vop2_data = vop2->data;
+	const struct vop2_vp_data *vp_data = &vop2_data->vp_data[cstate->crtc_id];
+	struct resource acm_regs;
+	u32 *acm_reg_base;
+	u32 val;
+	u32 vp_offset = (cstate->crtc_id * 0x100);
+	int ret;
+
+	if (!(vp_data->feature & VOP_FEATURE_POST_ACM))
+		return;
+
+	ret = ofnode_read_resource_byname(cstate->node, "acm_regs", &acm_regs);
+	if (ret) {
+		printf("failed to get acm regs\n");
+		return;
+	}
+	acm_reg_base = (u32 *)acm_regs.start;
+
+	/*
+	 * Black screen is displayed when acm bypass switched
+	 * between enable and disable. Therefore, disable acm
+	 * bypass by default after system boot.
+	 */
+	vop2_mask_write(vop2, RK3528_VP0_ACM_CTRL + vp_offset,
+			POST_ACM_BYPASS_EN_MASK, POST_ACM_BYPASS_EN_SHIFT, 0, false);
+
+	val = readl(acm_reg_base + RK3528_ACM_CTRL);
+	val &= ~BIT(1);
+	writel(val, acm_reg_base + RK3528_ACM_CTRL);
+}
+
 static int rockchip_vop2_of_get_gamma_lut(struct display_state *state,
 					  struct device_node *dsp_lut_node)
 {
@@ -3000,6 +3034,7 @@ static int vop2_initial(struct vop2 *vop2, struct display_state *state)
 	rockchip_vop2_gamma_lut_init(vop2, state);
 	rockchip_vop2_cubic_lut_init(vop2, state);
 	rockchip_vop2_sharp_init(vop2, state);
+	rockchip_vop2_acm_init(vop2, state);
 
 	return 0;
 }

commit e1946cbde9471f5c16ca3570f5542214ecbde8cb

video/drm: vop2: Fix screen was skewed to the left
Fixes: c54cc5d2df71 ("video/drm: vop2: Disable acm bypass when vop init")

Change-Id: Iab343ce53e7fe3992cf7cabfc2e6176c565e9d31
Signed-off-by: Algea Cao <algea.cao@rock-chips.com>

drivers/video/drm/rockchip_vop2.c
@@ -2910,7 +2910,6 @@ static void rockchip_vop2_acm_init(struct vop2 *vop2, struct display_state *stat
 	const struct vop2_vp_data *vp_data = &vop2_data->vp_data[cstate->crtc_id];
 	struct resource acm_regs;
 	u32 *acm_reg_base;
-	u32 val;
 	u32 vp_offset = (cstate->crtc_id * 0x100);
 	int ret;
 
@@ -2932,9 +2931,7 @@ static void rockchip_vop2_acm_init(struct vop2 *vop2, struct display_state *stat
 	vop2_mask_write(vop2, RK3528_VP0_ACM_CTRL + vp_offset,
 			POST_ACM_BYPASS_EN_MASK, POST_ACM_BYPASS_EN_SHIFT, 0, false);
 
-	val = readl(acm_reg_base + RK3528_ACM_CTRL);
-	val &= ~BIT(1);
-	writel(val, acm_reg_base + RK3528_ACM_CTRL);
+	writel(0, acm_reg_base + 0);
 }
 
 static int rockchip_vop2_of_get_gamma_lut(struct display_state *state,
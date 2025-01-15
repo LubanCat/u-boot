be1ae0c9cd7b14d0efa96e64a59f8f0ee667e287
Jon Lin <jon.lin@rock-chips.com>
2025-01-15 14:31:28 +0800

    mtd: spi-nor-ids: support ZB25Q256A
    Change-Id: Ie554e1972716e3c9aa7768de01d50493a8695264
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -570,5 +570,9 @@ const struct flash_info spi_nor_ids[] = {
 	/* NORMEM Microelectronics Co., Ltd. */
 	{ INFO("NM25Q128EVB", 0x522118, 0, 64 * 1024, 256, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 #endif
+#ifdef CONFIG_SPI_FLASH_ZBIT
+	/* Zbit Microelectronics Co., Ltd. */
+	{ INFO("ZB25Q256A", 0x5E4019, 0, 64 * 1024, 512, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES) },
+#endif
 	{ },
 };

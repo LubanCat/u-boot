LubanCat/u-boot

commit 9a60974db52600c71a5dd87807c97f03f87d285d

phy: rockchip: usbdp: Fix usb3 phy init for rockusb
Signed-off-by: Jianwei Zheng <jianwei.zheng@rock-chips.com>
Signed-off-by: william.wu <william.wu@rock-chips.com>
Change-Id: I47284e88afd4d539af1a13e3c8b875638111bc44

drivers/phy/phy-rockchip-usbdp.c
@@ -551,11 +551,10 @@ static int udphy_disable(struct rockchip_udphy *udphy)
 
 static int udphy_parse_lane_mux_data(struct rockchip_udphy *udphy, struct udevice *dev)
 {
-	const struct device_node *np = ofnode_to_np(dev->node);
-	struct property *prop;
+	const void *prop;
 	int ret, i, len, num_lanes;
 
-	prop = of_find_property(np, "rockchip,dp-lane-mux", &len);
+	prop = dev_read_prop(dev, "rockchip,dp-lane-mux", &len);
 	if (!prop) {
 		dev_dbg(dev, "failed to find dp lane mux, following dp alt mode\n");
 		udphy->mode = UDPHY_MODE_USB;
@@ -569,7 +568,7 @@ static int udphy_parse_lane_mux_data(struct rockchip_udphy *udphy, struct udevic
 		return -EINVAL;
 	}
 
-	ret = of_read_u32_array(np, "rockchip,dp-lane-mux", udphy->dp_lane_sel, num_lanes);
+	ret = dev_read_u32_array(dev, "rockchip,dp-lane-mux", udphy->dp_lane_sel, num_lanes);
 	if (ret) {
 		dev_err(dev, "get dp lane mux failed\n");
 		return -EINVAL;
@@ -887,6 +886,7 @@ int rockchip_u3phy_uboot_init(void)
 {
 	struct udevice *udev;
 	struct rockchip_udphy *udphy;
+	unsigned int val;
 	int ret;
 
 	ret = uclass_get_device_by_driver(UCLASS_PHY,
@@ -900,23 +900,45 @@ int rockchip_u3phy_uboot_init(void)
 	/* DP only or high-speed, disable U3 port */
 	udphy = dev_get_priv(udev->parent);
 	if (!(udphy->mode & UDPHY_MODE_USB) || udphy->hs) {
-		udphy_u3_port_disable(udphy, true);
-		return 0;
+		pr_err("%s: udphy mode not support usb3\n", __func__);
+		goto disable_u3;
 	}
 
-	return udphy_power_on(udphy, UDPHY_MODE_USB);
+	udphy->flip = false;
+	upphy_set_typec_default_mapping(udphy);
+
+	ret =  udphy_power_on(udphy, UDPHY_MODE_USB);
+	if (ret) {
+		pr_err("%s: udphy power on failed: %d\n", __func__, ret);
+		goto disable_u3;
+	}
+
+	ret = regmap_read_poll_timeout(udphy->pma_regmap,
+				       TRSV_LN0_MON_RX_CDR_DONE_OFFSET, val,
+				       val & TRSV_LN0_MON_RX_CDR_LOCK_DONE,
+				       200, 100);
+	if (ret) {
+		pr_err("%s: udphy rx cdr lock timeout\n", __func__);
+		goto disable_u3;
+	}
+
+	return 0;
+
+disable_u3:
+	udphy_u3_port_disable(udphy, true);
+
+	return -EOPNOTSUPP;
 }
 
 static int rockchip_udphy_probe(struct udevice *dev)
 {
-	const struct device_node *np = ofnode_to_np(dev->node);
 	struct rockchip_udphy *udphy = dev_get_priv(dev);
 	const struct rockchip_udphy_cfg *phy_cfgs;
 	int id, ret;
 
 	udphy->dev = dev;
 
-	id = of_alias_get_id(np, "usbdp");
+	dev_read_alias_seq(dev, &id);
 	if (id < 0)
 		id = 0;
 	udphy->id = id;

include/linux/usb/phy-rockchip-usbdp.h
@@ -67,4 +67,13 @@
 #define TRSV_LN2_MON_RX_CDR_DONE_OFFSET		0x1b84	/* trsv_reg06E1 */
 #define TRSV_LN2_MON_RX_CDR_LOCK_DONE		BIT(0)
 
+#if CONFIG_IS_ENABLED(PHY_ROCKCHIP_USBDP)
+int rockchip_u3phy_uboot_init(void);
+#else
+static inline int rockchip_u3phy_uboot_init(void)
+{
+	return -ENOTSUPP;
+}
+#endif
+
 #endif

commit 3e6af0e752c8f8d88b43672fb393f1a0a092a229

phy: rockchip: usbdp: fix phy device mismatch
Pass the PHY physical address to ensure that the correct PHY device
associated with the USB download controller can be found.

Fixes: 9a60974db526 ("phy: rockchip: usbdp: Fix usb3 phy init for rockusb")
Change-Id: Ic10a9ae570e3831e7b7acbc116d2a20dee51e920
Signed-off-by: Frank Wang <frank.wang@rock-chips.com>

board/rockchip/evb_rk3576/evb_rk3576.c
@@ -16,6 +16,7 @@ DECLARE_GLOBAL_DATA_PTR;
 #ifdef CONFIG_USB_DWC3
 #define CRU_BASE		0x27200000
 #define CRU_SOFTRST_CON47	0x0abc
+#define U3PHY_BASE		0x2b010000
 
 static struct dwc3_device dwc3_device_data = {
 	.maximum_speed = USB_SPEED_SUPER,
@@ -57,14 +58,14 @@ int board_usb_init(int index, enum usb_init_type init)
 
 	if (rkusb_switch_usb3_enabled()) {
 		dwc3_device_data.maximum_speed = USB_SPEED_SUPER;
-		ret = rockchip_u3phy_uboot_init();
+		ret = rockchip_u3phy_uboot_init(U3PHY_BASE);
 		if (ret) {
 			rkusb_force_to_usb2(true);
 			dwc3_device_data.maximum_speed = USB_SPEED_HIGH;
 		}
 	}
 #else
-	ret = rockchip_u3phy_uboot_init();
+	ret = rockchip_u3phy_uboot_init(U3PHY_BASE);
 	if (ret) {
 		rkusb_force_to_usb2(true);
 		dwc3_device_data.maximum_speed = USB_SPEED_HIGH;

board/rockchip/evb_rk3588/evb_rk3588.c
@@ -16,6 +16,7 @@ DECLARE_GLOBAL_DATA_PTR;
 #ifdef CONFIG_USB_DWC3
 #define CRU_BASE		0xfd7c0000
 #define CRU_SOFTRST_CON42	0x0aa8
+#define U3PHY_BASE		0xfed80000
 
 static struct dwc3_device dwc3_device_data = {
 	.maximum_speed = USB_SPEED_SUPER,
@@ -57,14 +58,14 @@ int board_usb_init(int index, enum usb_init_type init)
 
 	if (rkusb_switch_usb3_enabled()) {
 		dwc3_device_data.maximum_speed = USB_SPEED_SUPER;
-		ret = rockchip_u3phy_uboot_init();
+		ret = rockchip_u3phy_uboot_init(U3PHY_BASE);
 		if (ret) {
 			rkusb_force_to_usb2(true);
 			dwc3_device_data.maximum_speed = USB_SPEED_HIGH;
 		}
 	}
 #else
-	ret = rockchip_u3phy_uboot_init();
+	ret = rockchip_u3phy_uboot_init(U3PHY_BASE);
 	if (ret) {
 		rkusb_force_to_usb2(true);
 		dwc3_device_data.maximum_speed = USB_SPEED_HIGH;

drivers/phy/phy-rockchip-usbdp.c
@@ -11,6 +11,7 @@
 #include <dm/lists.h>
 #include <dm/of.h>
 #include <dm/of_access.h>
+#include <dm/uclass-internal.h>
 #include <generic-phy.h>
 #include <linux/bitfield.h>
 #include <linux/bitops.h>
@@ -1209,23 +1210,36 @@ static const struct phy_ops rockchip_u3phy_ops = {
 	.exit		= rockchip_u3phy_exit,
 };
 
-int rockchip_u3phy_uboot_init(void)
+int rockchip_u3phy_uboot_init(fdt_addr_t phy_addr)
 {
-	struct udevice *udev;
+	struct udevice *udev = NULL;
+	struct udevice *dev;
+	struct uclass *uc;
+	const struct driver *find_drv;
 	struct rockchip_udphy *udphy;
 	unsigned int val;
 	int ret;
 
-	ret = uclass_get_device_by_driver(UCLASS_PHY,
-					  DM_GET_DRIVER(rockchip_udphy_u3_port),
-					  &udev);
-	if (ret) {
-		pr_err("%s: get u3-port failed: %d\n", __func__, ret);
+	ret = uclass_get(UCLASS_PHY, &uc);
+	if (ret)
+		return ret;
+
+	find_drv = DM_GET_DRIVER(rockchip_udphy);
+	list_for_each_entry(dev, &uc->dev_head, uclass_node) {
+		if (dev->driver == find_drv && dev_read_addr(dev) == phy_addr) {
+			ret = uclass_get_device_tail(dev, 0, &udev);
+			break;
+		}
+	}
+
+	if (!udev || ret) {
+		ret = ret ? ret : -ENODEV;
+		pr_err("%s: get usb3-phy node failed: %d\n", __func__, ret);
 		return ret;
 	}
 
 	/* DP only or high-speed, disable U3 port */
-	udphy = dev_get_priv(udev->parent);
+	udphy = dev_get_priv(udev);
 	if (!(udphy->mode & UDPHY_MODE_USB) || udphy->hs) {
 		pr_err("%s: udphy mode not support usb3\n", __func__);
 		goto disable_u3;

include/linux/usb/phy-rockchip-usbdp.h
@@ -9,9 +9,9 @@
 #define __PHY_ROCKCHIP_USBDP_H_
 
 #if CONFIG_IS_ENABLED(PHY_ROCKCHIP_USBDP)
-int rockchip_u3phy_uboot_init(void);
+int rockchip_u3phy_uboot_init(fdt_addr_t phy_addr);
 #else
-static inline int rockchip_u3phy_uboot_init(void)
+static inline int rockchip_u3phy_uboot_init(fdt_addr_t phy_addr)
 {
 	return -ENOTSUPP;
 }
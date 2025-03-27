3c3bf9767125dfa6ad925b0726df0390ae5f2a4b
Joseph Chen <chenjh@rock-chips.com>
2025-03-27 15:38:17 +0800

    rockchip: rv1126b: Update arch cpu init
    - Add sdmmc0 unset iomux support.
    - Enable npu pd.
    - Enable tsadc phy.
    - Set usb phy to save power consumption:
      Assert USB3 PIPE PHY reset and set the USB 2.0 PHY Port1 to
      enter the sleep mode to save power consumption.
    - Enable usb3phy clamp:
      Enable the USB3PHY clamp in SPL to ensure that the USB3PHY is in the
      clamp state when starting from various scenarios.
    - Restore pipe phy status to default:
      Set the PHY status to the register's default value in case it is
      modified in other stages.
    - Support boot mcu.
    
    Signed-off-by: Xuhui Lin <xuhui.lin@rock-chips.com>
    Signed-off-by: Frank Wang <frank.wang@rock-chips.com>
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>
    Signed-off-by: Frank Wang <frank.wang@rock-chips.com>
    Signed-off-by: Xuhui Lin <xuhui.lin@rock-chips.com>
    Signed-off-by: Frank Wang <frank.wang@rock-chips.com>
    Signed-off-by: Jason Zhu <jason.zhu@rock-chips.com>
    Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
    Change-Id: I7299c7f0fa27c4d5430a2266e6b2f533267fe6b9

--- arch/arm/mach-rockchip/rv1126b/rv1126b.c
+++ arch/arm/mach-rockchip/rv1126b/rv1126b.c
@@ -17,11 +17,41 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
+/* CRU */
+#define CRU_BUS_BASE			0x20010000
+#define CRU_BUS_GATE_CON06		0x818
+#define CRU_BUS_SOFTRST_CON01		0x0a04
+#define CRU_PMU_BASE			0x20040000
+#define CRU_PMU_SOFTRST_CON03		0x0a0c
+#define PERI_CRU_BASE			0x20020000
+#define PERICRU_PERI_SOFTRST_CON01	0x0a04
+
+/* GRF */
+#define SYS_GRF_BASE			0x20100000
+#define TSADC_GRF_CON0			0x50
+#define TSADC_GRF_CON1			0x54
+#define TSADC_GRF_CON6			0x68
+#define GRF_JTAG_CON0			0x904
+
+#define PERI_GRF_BASE			0x20110000
+#define PERI_GRF_USB2HOSTPHY_CON0	0x001c
+#define PERI_GRF_USB3DRD_CON1		0x003C
+
+/* PMU */
+#define PMU_GRF_BASE			0x20130000
+#define PMU_GRF_SOC_CON0		0x0000
+#define PMU2_BASE			0x20838000
+#define PMU2_PWR_GATE_SFTCON0		0x0210
+
+/* GPIO/IOC */
+#define GPIO0_BASE			0x20600000
+#define GPIO_SWPORT_DR_L		0x00
+#define GPIO_SWPORT_DDR_L		0x08
+
 #define PMUIO0_IOC_BASE			0x201a0000
 #define GPIO0A_IOMUX_SEL_L		0x0
 #define GPIO0A_IOMUX_SEL_H		0x4
 #define GPIO0B_IOMUX_SEL_L		0x8
-#define GRF_JTAG_CON0			0x904
 
 #define VCCIO1_IOC_BASE			0x201b0000
 #define GPIO1A_IOMUX_SEL_L		0x20
@@ -40,7 +70,9 @@ DECLARE_GLOBAL_DATA_PTR;
 #define GPIO3B_IOMUX_SEL_H		0x6c
 #define GPIO3A_PULL			0x330
 
+/* SGRF/FIREWALL */
 #define SGRF_SYS_BASE			0x20220000
+#define SGRF_HPMCU_BOOT_ADDR		0x0c
 #define SGRF_SYS_AHB_SECURE_SGRF_CON	0x14
 #define SGRF_SYS_AXI_SECURE_SGRF_CON0	0x18
 #define FIREWALL_SLV_CON0		0x20
@@ -50,6 +82,10 @@ DECLARE_GLOBAL_DATA_PTR;
 #define FIREWALL_SLV_CON4		0x30
 #define FIREWALL_SLV_CON5		0x34
 #define OTP_SGRF_CON			0x1c
+
+#define SGRF_PMU_BASE			0x20230000
+#define SGRF_PMU_SOC_CON0		0x00
+#define SGRF_LPMCU_BOOT_ADDR		0x20
 
 #ifdef CONFIG_ARM64
 #include <asm/armv8/mmu.h>
@@ -89,24 +125,6 @@ void board_debug_uart_init(void)
 	/* No need to change uart in most time. */
 }
 
-#ifdef CONFIG_SPL_BUILD
-void rockchip_stimer_init(void)
-{
-	u32 reg;
-
-	/* If Timer already enabled, don't re-init it */
-	reg = readl(CONFIG_ROCKCHIP_STIMER_BASE + 0x4);
-	if (reg & 0x1)
-		return;
-#ifdef COUNTER_FREQUENCY
-	asm volatile("msr CNTFRQ_EL0, %0" : : "r" (COUNTER_FREQUENCY));
-#endif
-	writel(0xffffffff, CONFIG_ROCKCHIP_STIMER_BASE + 0x14);
-	writel(0xffffffff, CONFIG_ROCKCHIP_STIMER_BASE + 0x18);
-	writel(0x00010001, CONFIG_ROCKCHIP_STIMER_BASE + 0x04);
-}
-#endif
-
 void board_set_iomux(enum if_type if_type, int devnum, int routing)
 {
 	switch (if_type) {
@@ -116,11 +134,24 @@ void board_set_iomux(enum if_type if_type, int devnum, int routing)
 			writel(0xffff1111, VCCIO1_IOC_BASE + GPIO1A_IOMUX_SEL_H);
 			writel(0xf0f01010, VCCIO1_IOC_BASE + GPIO1B_IOMUX_SEL_L);
 		} else if (devnum == 1) {
+#if CONFIG_SPL_BUILD
+			/* set SDMMC D0-3/CMD/CLK to gpio and pull down */
+			writel(0xffff0000, VCCIO2_IOC_BASE + GPIO2A_IOMUX_SEL_L);
+			writel(0x00ff0000, VCCIO2_IOC_BASE + GPIO2A_IOMUX_SEL_H);
+			writel(0x00f00000, PMUIO0_IOC_BASE + GPIO0A_IOMUX_SEL_H);
+			writel(0x0fff0aaa, VCCIO2_IOC_BASE + GPIO2A_PULL);
+
+			/* SDMMC PWREN GPIO0A4 power down and power up */
+			writel(0x01000100, GPIO0_BASE + GPIO_SWPORT_DR_L);
+			writel(0x01000100, GPIO0_BASE + GPIO_SWPORT_DDR_L);
+			mdelay(50);
+			writel(0x01000000, GPIO0_BASE + GPIO_SWPORT_DR_L);
+#endif
+			/* set SDMMC D0-3/CMD/CLK and pull up */
 			writel(0xffff1111, VCCIO2_IOC_BASE + GPIO2A_IOMUX_SEL_L);
 			writel(0x00ff0011, VCCIO2_IOC_BASE + GPIO2A_IOMUX_SEL_H);
 			writel(0x00f00010, PMUIO0_IOC_BASE + GPIO0A_IOMUX_SEL_H);
-			/* Pull up */
-			writel(0x0cff0455, VCCIO2_IOC_BASE + GPIO2A_PULL);
+			writel(0x0fff0555, VCCIO2_IOC_BASE + GPIO2A_PULL);
 		} else if (devnum == 2) {
 			writel(0xffff1111, VCCIO3_IOC_BASE + GPIO3A_IOMUX_SEL_L);
 			writel(0x00ff0011, VCCIO3_IOC_BASE + GPIO3A_IOMUX_SEL_H);
@@ -160,11 +191,13 @@ void board_unset_iomux(enum if_type if_type, int devnum, int routing)
 			writel(0xffff0000, VCCIO1_IOC_BASE + GPIO1A_IOMUX_SEL_H);
 			writel(0xf0f00000, VCCIO1_IOC_BASE + GPIO1B_IOMUX_SEL_L);
 		} else if (devnum == 1) {
-			writel(0xffff0000, VCCIO2_IOC_BASE + GPIO2A_IOMUX_SEL_L);
+			/* SDMMC0_D2,D3 -> JTAG_TMS_M1, JTAG_TCK_M1 */
+			writel(0xffff4400, VCCIO2_IOC_BASE + GPIO2A_IOMUX_SEL_L);
+			/* Other SDMMC0 PINS -> GPIO */
 			writel(0x00ff0000, VCCIO2_IOC_BASE + GPIO2A_IOMUX_SEL_H);
 			writel(0x00f00000, PMUIO0_IOC_BASE + GPIO0A_IOMUX_SEL_H);
 			/* Pull down */
-			writel(0x0cff0000, VCCIO2_IOC_BASE + GPIO2A_PULL);
+			writel(0x0fff0aaa, VCCIO2_IOC_BASE + GPIO2A_PULL);
 		} else if (devnum == 2) {
 			writel(0xffff0000, VCCIO3_IOC_BASE + GPIO3A_IOMUX_SEL_L);
 			writel(0x00ff0000, VCCIO3_IOC_BASE + GPIO3A_IOMUX_SEL_H);
@@ -195,10 +228,57 @@ void board_unset_iomux(enum if_type if_type, int devnum, int routing)
 	}
 }
 
+#ifdef CONFIG_SPL_BUILD
+void rockchip_stimer_init(void)
+{
+	u32 reg;
+
+	/* If Timer already enabled, don't re-init it */
+	reg = readl(CONFIG_ROCKCHIP_STIMER_BASE + 0x4);
+	if (reg & 0x1)
+		return;
+#ifdef COUNTER_FREQUENCY
+	asm volatile("msr CNTFRQ_EL0, %0" : : "r" (COUNTER_FREQUENCY));
+#endif
+	writel(0xffffffff, CONFIG_ROCKCHIP_STIMER_BASE + 0x14);
+	writel(0xffffffff, CONFIG_ROCKCHIP_STIMER_BASE + 0x18);
+	writel(0x00010001, CONFIG_ROCKCHIP_STIMER_BASE + 0x04);
+}
+
+void spl_board_storages_fixup(struct spl_image_loader *loader)
+{
+	if (!loader)
+		return;
+
+	if (loader->boot_device == BOOT_DEVICE_MMC2)
+		/* Unset the sdmmc0 iomux */
+		board_unset_iomux(IF_TYPE_MMC, 1, 0);
+}
+
+int spl_fit_standalone_release(char *id, uintptr_t entry_point)
+{
+	if (!strcmp(id, "mcu0")) {
+		writel(0x1e001e0, CRU_BUS_BASE + CRU_BUS_SOFTRST_CON01);
+		writel(entry_point, SGRF_SYS_BASE + SGRF_HPMCU_BOOT_ADDR);
+		writel(0x1 << 20, SGRF_SYS_BASE + SGRF_SYS_AHB_SECURE_SGRF_CON);
+		writel(0x1e00000, CRU_BUS_BASE + CRU_BUS_SOFTRST_CON01);
+	} else if (!strcmp(id, "mcu1")) {
+		writel(0x1c001c, CRU_PMU_BASE + CRU_PMU_SOFTRST_CON03);
+		writel(entry_point, SGRF_PMU_BASE + SGRF_LPMCU_BOOT_ADDR);
+		writel(0x1 << 23, SGRF_PMU_BASE + SGRF_PMU_SOC_CON0);
+		writel(0x1c0000, CRU_PMU_BASE + CRU_PMU_SOFTRST_CON03);
+	}
+
+	return 0;
+}
+#endif
+
 #ifndef CONFIG_TPL_BUILD
 int arch_cpu_init(void)
 {
 #if defined(CONFIG_SPL_BUILD) || defined(CONFIG_SUPPORT_USBPLUG)
+	/* Enable npu pd */
+	writel(0x00010000, PMU2_BASE + PMU2_PWR_GATE_SFTCON0);
 	/* Set emmc master secure */
 	writel(0x10000, SGRF_SYS_BASE + SGRF_SYS_AHB_SECURE_SGRF_CON);
 	/* Set fspi master secure */
@@ -221,7 +301,26 @@ int arch_cpu_init(void)
 	writel(0xffff0000, SGRF_SYS_BASE + FIREWALL_SLV_CON5);
 	/* Set OTP to none secure mode */
 	writel(0x00020000, SGRF_SYS_BASE + OTP_SGRF_CON);
-#endif
+
+	/* Set usb3phy clamp enable */
+	writel(0x40000000, PMU_GRF_BASE + PMU_GRF_SOC_CON0);
+
+	/* Assert the pipe phy reset and de-assert when in use */
+	writel(0x00800080, PERI_CRU_BASE + PERICRU_PERI_SOFTRST_CON01);
+
+	/* Restore pipe phy status to default from phy */
+	writel(0xffff1100, PERI_GRF_BASE + PERI_GRF_USB3DRD_CON1);
+
+	/* Set the USB 2.0 PHY Port1 to enter the sleep mode to save power consumption */
+	writel(0x01ff01d1, PERI_GRF_BASE + PERI_GRF_USB2HOSTPHY_CON0);
+
+	/* Enable tsadc phy */
+	writel(0x01000000, CRU_BUS_BASE + CRU_BUS_GATE_CON06);
+	writel(0x80788028, SYS_GRF_BASE + TSADC_GRF_CON0);
+	writel(0xff000300, SYS_GRF_BASE + TSADC_GRF_CON6);
+	writel(0x00ff00a5, SYS_GRF_BASE + TSADC_GRF_CON1);
+	writel(0x01000100, SYS_GRF_BASE + TSADC_GRF_CON1);
+	writel(0x01000000, SYS_GRF_BASE + TSADC_GRF_CON1);
 
 #if defined(CONFIG_ROCKCHIP_EMMC_IOMUX)
 	board_set_iomux(IF_TYPE_MMC, 0, 0);
@@ -237,8 +336,7 @@ int arch_cpu_init(void)
 #if defined(CONFIG_MMC_DW_ROCKCHIP)
 	/* Set the sdmmc iomux and power cycle */
 	board_set_iomux(IF_TYPE_MMC, 1, 0);
-	/* Enable force_jtag */
-	writel(0x00080008, PMUIO0_IOC_BASE + GRF_JTAG_CON0);
+#endif
 #endif
 
 	return 0;

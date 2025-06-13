LubanCat/u-boot

commit b6bda7d5f40f8df92d3195bfab2b7ecbabd3e740

rockchip: spl: Fix bootargs append support for kernel boot
Change-Id: I4a0aea5587a70544b66493dc343c9a36a6c4731e
Signed-off-by: Xuhui Lin <xuhui.lin@rock-chips.com>

common/spl/spl_fit.c
@@ -667,20 +667,10 @@ static int spl_load_kernel_fit(struct spl_image_info *spl_image,
 
 		/* initial addr or entry point */
 		if (!strcmp(images[i], FIT_FDT_PROP)) {
-			spl_image->fdt_addr = (void *)image_info.load_addr;
-#ifdef CONFIG_SPL_AB
-			char slot_suffix[3] = {0};
-
-			if (!spl_get_current_slot(info->dev, "misc", slot_suffix))
-				spl_ab_bootargs_append_slot((void *)image_info.load_addr, slot_suffix);
-#endif
-
-#ifdef CONFIG_SPL_MTD_SUPPORT
-			struct blk_desc *desc = info->dev;
-
-			if (desc->devnum == BLK_MTD_SPI_NAND)
-				fdt_bootargs_append((void *)image_info.load_addr, mtd_part_parse(desc));
-#endif
+			if (spl_fdt_chosen_bootargs(info, (void *)image_info.load_addr)) {
+				printf("ERROR: Append bootargs failed\n");
+				return -EINVAL;
+			}
 		} else if (!strcmp(images[i], FIT_KERNEL_PROP)) {
 #if CONFIG_IS_ENABLED(OPTEE)
 			spl_image->entry_point_os = image_info.load_addr;

arch/arm/include/asm/arch-rockchip/rk_meta.h
@@ -122,4 +122,5 @@ struct cmdline_info {
 	uint32_t crc32;
 };
 
+void rk_meta_bootargs_append(void *fdt);
 #endif

arch/arm/mach-rockchip/spl.c
@@ -9,11 +9,14 @@
 #include <boot_rkimg.h>
 #include <debug_uart.h>
 #include <dm.h>
+#include <envf.h>
 #include <key.h>
 #include <led.h>
 #include <misc.h>
+#include <mtd_blk.h>
 #include <ram.h>
 #include <spl.h>
+#include <spl_ab.h>
 #include <optee_include/OpteeClientInterface.h>
 #include <power/fuel_gauge.h>
 #include <asm/arch/bootrom.h>
@@ -27,6 +30,7 @@
 #include <asm/io.h>
 #include <asm/arch/param.h>
 #include <asm/arch/rk_hwid.h>
+#include <asm/arch/rk_meta.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -533,6 +537,75 @@ int spl_find_hwid_dtb(const char *fdt_name)
 	return hwid_dtb_is_available(fdt_name);
 }
 #endif
+
+int spl_fdt_chosen_bootargs(struct spl_load_info *info, void *fdt)
+{
+	__maybe_unused struct blk_desc *desc = info->dev;
+
+#ifdef CONFIG_SPL_AB
+	char slot_suffix[3] = {0};
+
+	if (!spl_get_current_slot(desc, "misc", slot_suffix))
+		spl_ab_bootargs_append_slot(fdt, slot_suffix);
+#endif
+
+#ifdef CONFIG_SPL_ENVF
+	char *part_type[] = { "mtdparts", "blkdevparts" };
+	char *env = NULL;
+	char *part_list;
+	int id = 0, ret = 0;
+
+	env = envf_get(desc, part_type[id]);
+	if (!env)
+		env = envf_get(desc, part_type[++id]);
+	if (env) {
+		if (!strstr(env, part_type[id])) {
+			part_list = calloc(1, strlen(env) + strlen(part_type[id]) + 2);
+			if (part_list) {
+				strcat(part_list, part_type[id]);
+				strcat(part_list, "=");
+				strcat(part_list, env);
+			}
+		} else {
+			part_list = env;
+		}
+		ret = fdt_bootargs_append(fdt, part_list);
+		if (ret) {
+			printf("Append parts info to bootargs fail");
+			return ret;
+		}
+		debug("## parts: %s\n\n", part_list);
+
+		env = envf_get(desc, "sys_bootargs");
+		env = env + strlen("sys_bootargs=");
+		if (env) {
+			ret = fdt_bootargs_append(fdt, env);
+			if (ret) {
+				printf("Append sys_bootargs to bootargs fail");
+				return ret;
+			}
+			debug("## sys_bootargs: %s\n\n", env);
+		}
+	}
+#endif
+#ifdef CONFIG_MTD_BLK
+	if (!env && desc->if_type == IF_TYPE_MTD) {
+		char *mtd_par_info = mtd_part_parse(desc);
+
+		ret = fdt_bootargs_append(fdt, mtd_par_info);
+		if (ret) {
+			printf("Append mtdparts info to bootargs fail");
+			return ret;
+		}
+		debug("## mtdparts: %s\n\n", mtd_par_info);
+	}
+#endif
+#ifdef CONFIG_ROCKCHIP_META
+	rk_meta_bootargs_append(fdt);
+#endif
+
+	return 0;
+}
 #endif
 
 void spl_perform_fixups(struct spl_image_info *spl_image)

include/spl.h
@@ -353,6 +353,10 @@ void spl_fdt_fixup_memory(struct spl_image_info *spl_image);
  * spl_find_hwid_dtb() - Support select kernel dtb based on HW-ID
  */
 int spl_find_hwid_dtb(const char *fdt_name);
+/**
+ * spl_fdt_chosen_bootargs() - Support append bootargs into kernel fdt chosen node
+ */
+int spl_fdt_chosen_bootargs(struct spl_load_info *info, void *fdt);
 #endif
 
 #endif

commit f11415a82eb7ada2453d6f82de5aa9ff34d9b44c

rockchip: spl: Correct bootargs append support for kernel boot
Fixes: b6bda7d5f40 (rockchip: spl: Fix bootargs append support for kernel boot)

Change-Id: I3fdea470d87966e538d837910f60f1b1065489f3
Signed-off-by: Xuhui Lin <xuhui.lin@rock-chips.com>

common/spl/spl_fit.c
@@ -667,6 +667,7 @@ static int spl_load_kernel_fit(struct spl_image_info *spl_image,
 
 		/* initial addr or entry point */
 		if (!strcmp(images[i], FIT_FDT_PROP)) {
+			spl_image->fdt_addr = (void *)image_info.load_addr;
 			if (spl_fdt_chosen_bootargs(info, (void *)image_info.load_addr)) {
 				printf("ERROR: Append bootargs failed\n");
 				return -EINVAL;
3d052225df8c1fb416f2558fbf0d3fd9b7b082da
min zhang <zhangmin1984121@gmail.com>
2026-03-02 21:41:42 -0800

    add more prints in scripts to help understand generating images

--- scripts/fit.sh
+++ scripts/fit.sh
@@ -14,21 +14,25 @@ if [ ! -z "${ARG_VALIDATE}" ]; then
 else
 	fit_raw_compile
 	if [ ! -z "${ARG_RECOVERY_IMG}" ]; then
+		echo "min fit.sh -> ARG_RECOVERY_IMG=${ARG_RECOVERY_IMG}"
 		fit_gen_recovery_itb
 		fit_gen_recovery_img
 	fi
 	# "--boot_img_dir" is for U-Boot debug only
 	if [ ! -z "${ARG_BOOT_IMG}" -o ! -z "${ARG_BOOT_IMG_DIR}" ]; then
+		echo "min fit.sh -> ARG_BOOT_IMG=${ARG_BOOT_IMG}, ARG_BOOT_IMG_DIR=${ARG_BOOT_IMG_DIR}"
 		fit_gen_boot_itb
 		fit_gen_boot_img
 	fi
 
 	if [ ! -z "${ARG_INI_TRUST}" ]; then
+		echo "min fit.sh -> ARG_INI_TRUST=${ARG_INI_TRUST}"
 		fit_gen_uboot_itb
 		fit_gen_uboot_img
 	fi
 
 	if [ ! -z "${ARG_INI_LOADER}" ]; then
+		echo "min fit.sh -> ARG_INI_LOADER=${ARG_INI_LOADER}"
 		fit_gen_loader
 	fi
 
